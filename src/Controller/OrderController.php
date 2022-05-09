e85061fa1a1f6f74156b096e53c901633026e2e8
johnsonjw48 <73818994+johnsonjw48@users.noreply.github.com>
2022-05-09 21:45:03 +0200

    order status

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -9,6 +9,7 @@ use App\Repository\OrderDetailsRepository;
 use App\Repository\OrderRepository;
 use App\Repository\ProduitsRepository;
 use DateTime;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -58,13 +59,18 @@ class OrderController extends AbstractController
         }
 
    
+       if(count($cart) === 0){
+                return $this->redirectToRoute('app_cart');
+       }else{
+                return $this->render('order/index.html.twig', [
+                    'form'=>$form->createView(),
+                    'total'=>$total,
+                    'cart'=>$cart
+    ]);
+       }
 
    
-        return $this->render('order/index.html.twig', [
-                'form'=>$form->createView(),
-                'total'=>$total,
-                'cart'=>$cart
-        ]);
+       
     }
 
     /**
@@ -73,6 +79,8 @@ class OrderController extends AbstractController
     public function add(OrderRepository $orderRepository, OrderDetailsRepository $orderDetailsRepository,Request $request, SessionInterface $session, ProduitsRepository $produitsRepository): Response
     {
         $em = $this->getDoctrine()->getManager();
+        $em->getConnection()->beginTransaction(); 
+
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
@@ -115,7 +123,9 @@ class OrderController extends AbstractController
   ##### Retour au formulaire
 
   if ($form->isSubmitted() && $form->isValid()) {
-    $date = new DateTime();
+
+            try {
+                 $date = new DateTime();
     $carriers= $form->get('carriers')->getData();
     $address= $form->get('addresses')->getData();
     $delivery = $address->getFirstName().' '.$address->getLastName();
@@ -139,8 +149,9 @@ class OrderController extends AbstractController
     $order->setDelivery($delivery);
     $order->setIsPaid(1);
 
-    $orderRepository->add($order);
+    $em->persist($order);
 
+    $cancel = 0;
 
        ### Enregistrer les produits en bdd
        foreach ($cart as $prod) {
@@ -155,26 +166,72 @@ class OrderController extends AbstractController
 
         $item->setStock($newStock);
 
-
+        if ($newStock < 0) {
+            $cancel++;
+        }
 
         $orderDetails->setPrice($prod['product']->getPrice());
         $orderDetails->setTotal($prod['product']->getPrice() * $prod['quantity']);
-        $orderDetailsRepository->add($orderDetails);
-        $produitsRepository->add($item);
+        $em->persist($orderDetails);
+        $em->persist($item);
+
+        
     }
 
+    if ($cancel > 0) {
+        throw new Exception('Commande annuler car Stock insuffisant');
+    }
+
+
+    $em->flush();
+    $em->getConnection()->commit();
+
     $session->remove('panier');
+  
+   
    
         return $this->render('order/add.html.twig', [
             'cart'=>$cart,
             'total'=>$total,
             'carrier'=>$form->get('carriers')->getData(),
             'delivery'=>$delivery
-            ]);
-        }
+        ]);
+            } catch (Exception $e) {
+                $em->getConnection()->rollBack();
 
-        return $this->redirectToRoute('app_cart');
+                return $this->render('order/cancel.html.twig', [
+                    'message' => $e->getMessage()
+                ]);
+            }
+
     }
 
+        return $this->redirectToRoute('app_cart');
+  }
+
+
+  /**
+   * @Route("/account/commands", name="account_commands")
+   */
+  public function myOrders(): Response
+  {
+
+      return $this->render('order/command.html.twig', []);
+  }
+
+    /**
+   * @Route("/account/commands/{id}", name="account_commands_details")
+   */
+  public function myOrderDetails($id, OrderDetailsRepository $orderDetailsRepository): Response
+  {
+
+    
+      return $this->render('order/details.html.twig', [
+          'order'=> $orderDetailsRepository->findBy(
+            array('command' => $id),        // $where 
+         
+        ),
+      ]);
+  }
 
 }
